8b21831bb27b8b402f982ba26e977ad1fecec69a
khalid0931 <mutiarif53@gmail.com>
2026-03-10 06:12:41 +0700

    first commit

---
+++ BMI.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+float hitungBMI(float berat,float tinggi){
+    return berat/(tinggi*tinggi);
+}
+
+string Status_BMI(float BMI){
+    if (BMI < 18,5){
+        return 
+    }
+    
+}
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+    
+
+    cout <<"masukan nilai bahasa inggris = ";
+    cin >> nilBI;
+    cout <<"masukan nilai matematika = ";
+    cin >> nilMT;
+    
+    rerata = Rata_rata(nilBI,nilMT);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
+
+}
